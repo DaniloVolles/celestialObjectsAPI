111ac493b518eb850a888b9a7956ff4b8e787521
Danilo Volles <danilo.volles@outlook.com>
2025-05-01 13:12:14 -0300

    InitialCommit

---
+++ src/main/java/com/danilo/volles/celestial/objects/api/CelestialObjectsApiApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.volles.celestial.objects.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelestialObjectsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelestialObjectsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danilo/volles/celestial/objects/api/CelestialObjectsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.volles.celestial.objects.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestialObjectsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
